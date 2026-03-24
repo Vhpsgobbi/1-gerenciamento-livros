21bcb67223696f6529fa49e06e8a595e097c0ae6
VictorSgobbi <victorhugosgobbi@gmail.com>
2026-03-23 23:36:03 -0300

    Criação da entidade Livro

---
+++ src/main/java/com/example/demo/entity/Livro.java
@@ -0,0 +1,56 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Livro")
+@Table(name = "tb_livro")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nm_titulo")
+    private String titulo;
+
+    @Column(name = "nm_autor")
+    private String autor;
+
+    @Column(name = "ano_publicacao")
+    private Integer anoPublicacao;
+
+    public Livro() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(Integer anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+}
